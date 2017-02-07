9b1064991f5946c3cc644e1c0287a7db25204753
asbjornh <asbjornh@gmail.com>
2017-02-07 22:57:27 +0100

    added socket.io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({
-	perMessageDeflate: false,
-	port: 80
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+io.on('connection', function(client){
+	client.on('event', function(data){});
+	client.on('disconnect', function(){});
 });
+server.listen(3000);
