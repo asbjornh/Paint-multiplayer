9811c4b121548d3f120b429f464f745d2e5c365d
asbjornh <asbjornh@gmail.com>
2017-02-09 18:59:04 +0100

    new server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const server = require('http').createServer()  
+const http = require("http")
+// const server = http.createServer()  
 
-server.listen(80, () => {  
-	console.log(`server is listening on ${port}`)
-})
+// server.listen(process.env.port, () => {  
+// 	console.log('server is listening')
+// })
+
+http.createServer( (request, response) => {
+	response.writeHead(200, {
+		"Content-type": "text/plain"
+	})
+	response.write("Hello")
+	response.end
+}).listen(process.env.port)
