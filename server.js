23479c0c4ef901bd1db00484c2b9519489afb181
asbjornh <asbjornh@gmail.com>
2017-02-09 19:13:35 +0100

    added express

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
+const port = process.env.port
 const http = require("http")
-// const server = http.createServer()  
+const app = require("express")
+// const server = http.createServer()
 
 // server.listen(process.env.port, () => {  
 // 	console.log('server is listening')
 // })
 
-http.createServer( (request, response) => {
-	response.writeHead(200, {
-		"Content-type": "text/plain"
-	})
-	response.write("Hello")
-	response.end
-	console.log("request", request);
-}).listen(process.env.port)
+// http.createServer( (request, response) => {
+// 	response.writeHead(200, {
+// 		"Content-type": "text/plain"
+// 	})
+// 	response.write("Hello")
+// 	response.end
+// 	console.log("request", request);
+// }).listen(port)
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html")
+})
+
+app.get(/^(.+)$/, (req, res) => {
+	console.log('static file request : ' + req.params)
+	res.sendfile( __dirname + req.params[0])
+})
+
+app.listen(port, () => {
+	console.log("listening on " + port)
+})
